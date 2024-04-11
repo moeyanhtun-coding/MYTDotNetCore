d72780f72559a030178e993e719ee9cc71aaa20f
Moe Yan Htun <moeyan227970@gmail.com>
2024-04-11 21:08:37 +0630

    insert function

---
+++ MYTDotNetCore.ConsoleApp/AdoDotNetExample.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MYTDotNetCore.ConsoleApp
+{
+    internal class AdoDotNetExample
+    {
+        private readonly SqlConnectionStringBuilder _sqlConnectionStringBuilder = new SqlConnectionStringBuilder()
+        {
+            DataSource = "AaronDGrant\\MSSQL", // server name 
+            InitialCatalog = "MYTDotNetCore", // database name
+            UserID = "sa", // username
+            Password = "sa@123", // db password
+        };
+        public void Read()
+        {
+            SqlConnection connection = new SqlConnection(_sqlConnectionStringBuilder.ConnectionString);
+
+            connection.Open();
+            Console.WriteLine("Connection Open");
+
+            string query = "select * from Tbl_Blog";
+            SqlCommand cmd = new SqlCommand(query, connection);
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sqlDataAdapter.Fill(dt);
+
+            connection.Close();
+            Console.WriteLine("Connection Close");
+
+            // Dataset => DataTable
+            // DataTable => DataRow
+            // DataRow => DataColumn
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                Console.WriteLine("Blog Id => " + dr["BlogId"]);
+                Console.WriteLine("Blog Title => " + dr["BlogTitle"]);
+                Console.WriteLine("Blog Author => " + dr["BlogAuthor"]);
+                Console.WriteLine("Blog Content => " + dr["BlogContent"]);
+                Console.WriteLine("---------------");
+            }
+
+        }
+
+        public void Create(string title, string author, string content)
+        {
+            SqlConnection connection = new SqlConnection(_sqlConnectionStringBuilder.ConnectionString);
+            connection.Open();
+
+            string query = @" INSERT INTO [dbo].[Tbl_Blog]
+           ([BlogTitle]
+           ,[BlogAuthor]
+           ,[BlogContent])
+     VALUES
+           (@BlogTitle
+           ,@BlogAuthor
+           ,@BlogContent)";
+            SqlCommand cmd = new SqlCommand(query, connection);
+            cmd.Parameters.AddWithValue("@BlogTitle", title);
+            cmd.Parameters.AddWithValue("@BlogAuthor", author);
+            cmd.Parameters.AddWithValue("@BlogContent", content);
+            int result = cmd.ExecuteNonQuery();
+
+            connection.Close();
+
+            string message = result > 0 ? "Data Insert Success" : "Error Occur";
+            Console.WriteLine(message);
+        }
+    }
+}
--- MYTDotNetCore.ConsoleApp/Program.cs
+++ MYTDotNetCore.ConsoleApp/Program.cs
@@ -1,46 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Data;
 using System.Data.SqlClient;
+using MYTDotNetCore.ConsoleApp;
 
 Console.WriteLine("Hello, World!");
 
 
 // => c# => db connection 
-SqlConnectionStringBuilder stringBuilder = new SqlConnectionStringBuilder();
 
-stringBuilder.DataSource = "AaronDGrant\\MSSQL"; // server name 
-stringBuilder.InitialCatalog = "MYTDotNetCore"; // database name
-stringBuilder.UserID = "sa"; // username
-stringBuilder.Password = "sa@123"; // db password
-
-SqlConnection connection = new SqlConnection(stringBuilder.ConnectionString);
-
-connection.Open();
-Console.WriteLine("Connection Open");
-
-string query = "select * from Tbl_Blog";
-SqlCommand cmd = new SqlCommand(query, connection);
-SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
-DataTable dt = new DataTable();
-sqlDataAdapter.Fill(dt);
-
-connection.Close();
-Console.WriteLine("Connection Close");
-
-// Dataset => DataTable
-// DataTable => DataRow
-// DataRow => DataColumn
-
-foreach (DataRow dr in dt.Rows)
-{
-    Console.WriteLine("Blog Id => " + dr["BlogId"]);
-    Console.WriteLine("Blog Title => " + dr["BlogTitle"]);
-    Console.WriteLine("Blog Author => " + dr["BlogAuthor"]);
-    Console.WriteLine("Blog Content => " + dr["BlogContent"]);
-    Console.WriteLine("---------------");
-}
-
+AdoDotNetExample adoDotNetExample = new AdoDotNetExample();
+adoDotNetExample.Read();
+//adoDotNetExample.Create("this is title", "this is Author", "this is Content");
 // Ado.Net Read
 // CRUD
 
- Console.ReadKey(); 
+Console.ReadKey(); 
