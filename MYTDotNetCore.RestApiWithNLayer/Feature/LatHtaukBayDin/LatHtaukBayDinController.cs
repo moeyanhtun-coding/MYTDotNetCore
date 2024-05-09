0533ae519fe9dc7b51f73b9592e855b134be9837
Moe Yan Htun <moeyan227970@gmail.com>
2024-05-09 21:35:54 +0630

    [Completed] ConsoleAppHttpClient & LatHtaukBayDin

---
+++ MYTDotNetCore.RestApiWithNLayer/Feature/LatHtaukBayDin/LatHtaukBayDinController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
+using MYTDotNetCore.RestApiWithNLayer.Models;
+using Newtonsoft.Json;
+
+namespace MYTDotNetCore.RestApiWithNLayer.Feature.LatHtaukBayDin
+{
+
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LatHtaukBayDinController : ControllerBase
+    {
+        private async Task<Models.LatHtaukBayDin> GetDataAsync()
+        {
+            string jsonStr = await System.IO.File.ReadAllTextAsync("data.json");
+            var model = JsonConvert.DeserializeObject<Models.LatHtaukBayDin>(jsonStr);
+            return model;
+        }
+
+        [HttpGet("questions")]
+        public async Task<IActionResult> Question()
+        {
+            var model = await GetDataAsync();
+            return Ok(model.questions);
+        }
+        
+        [HttpGet("numberList")]
+        public async Task<IActionResult> NumberLists()
+        {
+            var model = await GetDataAsync();
+            return Ok(model.numberList);
+        }
+
+        [HttpGet("answer/{questionNo}/{no}")]
+        public async Task<IActionResult> Answer(int questionNo, int no)
+        {
+            var model = await GetDataAsync();
+            if (!questionNo.Equals(model.questions.Length) && no > 10)
+            {
+                return NotFound("Question or Number Not Found");
+            }
+            return Ok(model.answers.FirstOrDefault(x => x.questionNo == questionNo && x.answerNo == no));
+        }
+
+    }
+
+
+}
---
+++ MYTDotNetCore.RestApiWithNLayer/Models/LatHtaukBayDin.cs
@@ -0,0 +1,24 @@
+﻿namespace MYTDotNetCore.RestApiWithNLayer.Models
+{
+
+    public class LatHtaukBayDin
+    {
+        public Question[] questions { get; set; }
+        public Answer[] answers { get; set; }
+        public string[] numberList { get; set; }
+    }
+
+    public class Question
+    {
+        public int questionNo { get; set; }
+        public string questionName { get; set; }
+    }
+
+    public class Answer
+    {
+        public int questionNo { get; set; }
+        public int answerNo { get; set; }
+        public string answerResult { get; set; }
+    }
+
+}
