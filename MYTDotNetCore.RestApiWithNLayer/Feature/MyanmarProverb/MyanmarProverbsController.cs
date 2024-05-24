a7287846bd3a9af53557fa5690edc4c464564345
Moe Yan Htun <moeyan227970@gmail.com>
2024-05-24 11:58:53 +0630

    [Complete] Myanmar Proverbs Project API

---
+++ MYTDotNetCore.RestApiWithNLayer/Feature/MyanmarProverb/MyanmarProverbsController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MYTDotNetCore.RestApiWithNLayer.Models;
+using Newtonsoft.Json;
+using static MYTDotNetCore.RestApiWithNLayer.Models.MyanmarProverbModel;
+
+namespace MYTDotNetCore.RestApiWithNLayer.Feature.MyanmarProverb
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MyanmarProverbsController : ControllerBase
+    {
+        private async Task<MainDto> GetDataFromApi()
+        {
+            var jsonStr = await System.IO.File.ReadAllTextAsync("MyanmarProverb.json");
+            var model = JsonConvert.DeserializeObject<MainDto>(jsonStr);
+            return model!;
+        }
+
+        [HttpGet("MyanmarProverb")]
+        public async Task<IActionResult> Get()
+        {
+            var model = await GetDataFromApi();
+            return Ok(model.Tbl_MMProverbsTitle);
+        }
+
+        [HttpGet("MyanmarProverb{titleName}")]
+        public async Task<IActionResult> Get(string titleName)
+        {
+            var model = await GetDataFromApi();
+            var item = model.Tbl_MMProverbsTitle.FirstOrDefault(x => x.TitleName == titleName);
+            if (item is null)
+                return NotFound();
+
+            var titleId = item.TitleId;
+            var result = model.Tbl_MMProverbs.Where(x => x.TitleId == titleId);
+            List<Tbl_MmproverbsHead> lst = result
+                .Select(x => new Tbl_MmproverbsHead
+                {
+                    TitleId = x.TitleId,
+                    ProverbId = x.ProverbId,
+                    ProverbName = x.ProverbName
+                })
+                .ToList();
+            return Ok(lst);
+        }
+
+        [HttpGet("MyanmarProverb/{titleId}/{proverbId}")]
+        public async Task<IActionResult> Get(int titleId, int proverbId)
+        {
+            var model = await GetDataFromApi();
+            var item = model.Tbl_MMProverbs.Where(x => x.TitleId == titleId && x.ProverbId == proverbId);
+            if (item is null)
+                return NotFound("No Data Found");
+            return Ok(item);
+        }
+    }
+}
---
+++ MYTDotNetCore.RestApiWithNLayer/Models/MyanmarProverbModel.cs
@@ -0,0 +1,34 @@
+﻿namespace MYTDotNetCore.RestApiWithNLayer.Models;
+
+public class MyanmarProverbModel
+{
+
+    public class MainDto
+    {
+        public Tbl_Mmproverbstitle[] Tbl_MMProverbsTitle { get; set; }
+        public Tbl_Mmproverbs[] Tbl_MMProverbs { get; set; }
+        public Tbl_MmproverbsHead[] Tbl_MmproverbsHeads { get; set; } 
+    }
+
+    public class Tbl_Mmproverbstitle
+    {
+        public int TitleId { get; set; }
+        public string TitleName { get; set; }
+    }
+
+    public class Tbl_MmproverbsHead
+    {
+        public int TitleId { get; set; }
+        public int ProverbId { get; set; }
+        public string ProverbName { get; set; }
+    }
+
+    public class Tbl_Mmproverbs
+    {
+        public int TitleId { get; set; }
+        public int ProverbId { get; set; }
+        public string ProverbName { get; set; }
+        public string ProverbDesp { get; set; }
+    }
+
+}
