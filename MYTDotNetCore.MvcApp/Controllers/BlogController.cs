cd21245352d7d70372beddca364600f359c81f44
Moe Yan Htun <moeyan227970@gmail.com>
2024-06-02 10:43:30 +0630

    [In Progress] MVC Blog CRUD

--- MYTDotNetCore.MvcApp/Controllers/BlogController.cs
+++ MYTDotNetCore.MvcApp/Controllers/BlogController.cs
@@ -17,7 +17,34 @@ namespace MYTDotNetCore.MvcApp.Controllers
         public IActionResult BlogIndex()
         {
             List<BlogModel> lst = _context.Blogs.ToList();
-            return View("BlogIndex",lst);
+            return View("BlogIndex", lst);
+        }
+
+        [ActionName("Edit")]
+        public IActionResult BlogEdit(int id)
+        {
+            var item = _context.Blogs.FirstOrDefault(x => x.BlogId == id);
+            if (item is null)
+            {
+                return Redirect("/Blog");
+            }
+            return View("BlogEdit", item);
+        }
+
+        [ActionName("Update")]
+        public IActionResult BlogUpdate(int id, BlogModel blog)
+        {
+            var item = _context.Blogs.FirstOrDefault(x => x.BlogId == id);
+            if (item is null)
+            {
+                return Redirect("/Blog");
+            }
+            item.BlogTitle = blog.BlogTitle;
+            item.BlogAuthor = blog.BlogAuthor;
+            item.BlogContent = blog.BlogContent;
+            _context.SaveChanges();
+
+            return Redirect("/Blog");
         }
     }
 }
