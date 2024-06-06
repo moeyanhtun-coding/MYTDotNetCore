fdca7145942b0adb1f5b3c8dc9a3b57944db4661
Moe Yan Htun <moeyan227970@gmail.com>
2024-06-06 12:49:54 +0630

    [In Progress] MVC Pagination

---
+++ MYTDotNetCore.MvcApp/Controllers/BlogPaginationController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MYTDotNetCore.MvcApp.Db;
+using MYTDotNetCore.MvcApp.Models;
+
+namespace MYTDotNetCore.MvcApp.Controllers
+{
+    public class BlogPaginationController : Controller
+    {
+        private readonly AppDbContext _db = new AppDbContext();
+
+        [ActionName("Index")]
+        public IActionResult BlogIndex(int pageNo = 1, int pageSize = 10)
+        {
+            var lst = _db.Blogs.Skip((pageNo - 1) * pageSize).Take(pageSize).ToList();
+
+            int rowCount = _db.Blogs.Count();
+            int pageCount = rowCount / pageSize;
+            if (rowCount % pageSize > 0)
+            {
+                pageCount++;
+            }
+            if (pageNo > pageCount)
+            {
+                return Redirect("/Blog");
+            }
+            BlogResponseModel model = new();
+            model.Data = lst;
+            model.PageSize = pageSize;
+            model.PageNo = pageNo;
+            model.PageCount = pageCount;
+
+            return View("BlogIndex",model);
+        }
+    }
+}
+ 
