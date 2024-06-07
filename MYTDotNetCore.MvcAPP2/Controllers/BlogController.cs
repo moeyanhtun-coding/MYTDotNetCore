26773edf89f65b755504cc3d516427c0ebb4f33a
Moe Yan Htun <moeyan227970@gmail.com>
2024-06-07 21:26:17 +0630

    -

--- MYTDotNetCore.MvcAPP2/Controllers/BlogController.cs
+++ MYTDotNetCore.MvcAPP2/Controllers/BlogController.cs
@@ -1,21 +1,60 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using MYTDotNetCore.MvcAPP2.Models;
+using Newtonsoft.Json;
+using static System.Net.Mime.MediaTypeNames;
 
-namespace MYTDotNetCore.MvcAPP2.Controllers
+namespace MYTDotNetCore.MvcAPP2.Controllers;
+
+public class BlogController : Controller
 {
-    public class BlogController : Controller
+    private readonly HttpClient _httpClient;
+
+    public BlogController(HttpClient httpClient)
     {
-        private readonly HttpClient _httpClient;
+        _httpClient = httpClient;
+    }
 
-        public BlogController(HttpClient httpClient)
+    [ActionName("Index")]
+    public async Task<IActionResult> BlogIndexAsync(int pageNo = 1, int pageSize = 10)
+    {
+        BlogResponseModel model = new BlogResponseModel();
+        HttpResponseMessage response = await _httpClient.GetAsync($"api/blog/{pageNo}/{pageSize}");
+        if (response.IsSuccessStatusCode)
         {
-            _httpClient = httpClient;
+            var jsonStr = await response.Content.ReadAsStringAsync();
+            model = JsonConvert.DeserializeObject<BlogResponseModel>(jsonStr)!;
         }
+        return View("BlogIndex", model);
+    }
 
-        [ActionName("Index")]
-        public async Task<IActionResult> BlogIndexAsync()
-        {
-           var response = await _HttpClient.GetAsync($"api/blog")
-            return View();
-        }
+    [ActionName("Create")]
+    public IActionResult BlogCreate()
+    {
+        return View("BlogCreate");
+    }
+
+    [HttpPost]
+    [ActionName("Save")]
+    public async Task<IActionResult> BlogSave(BlogModel blog)
+    {
+        var content = new StringContent(
+            JsonConvert.SerializeObject(blog),
+            Encoding.UTF8,
+            Application.Json
+        );
+        await _httpClient.PostAsync("api/blog", content);
+        return Redirect("/Blog");
+    }
+
+    [ActionName("Edit")]
+    public async Task<IActionResult> BlogEdit(int id)
+    {
+        HttpResponseMessage response = await _httpClient.GetAsync($"api/blog/{id}");
+        if (!response.IsSuccessStatusCode)
+            return Redirect("/Blog");
+        var jsonStr = await response.Content.ReadAsStringAsync();
+        BlogModel model = JsonConvert.DeserializeObject<BlogModel>(jsonStr)!;
+        return View("BlogEdit", model);
     }
 }
---
+++ MYTDotNetCore.MvcAPP2/Models/BlogModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MYTDotNetCore.MvcAPP2.Models;
+
+[Table("Tbl_Blog")]
+public class BlogModel
+{
+    [Key]
+    public int BlogId { get; set; }
+    public string? BlogTitle { get; set; }
+    public string? BlogAuthor { get; set; }
+    public string? BlogContent { get; set; }
+}
+
+public class BlogResponseModel
+{
+    public int PageNo { get; set; }
+    public int PageSize { get; set; }
+    public int PageCount { get; set; }
+    public bool IsEndOfPage => PageNo == PageCount;
+    public List<BlogModel> Data { get; set; }
+}
