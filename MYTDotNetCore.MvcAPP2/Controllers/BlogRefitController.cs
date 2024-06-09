adca7610d24f7e362316bd8a0ae475e795836d93
Moe Yan Htun <moeyan227970@gmail.com>
2024-06-09 17:05:48 +0630

    [Complete] MVC and Backend Refit

---
+++ MYTDotNetCore.MvcAPP2/Controllers/BlogRefitController.cs
@@ -0,0 +1,59 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using MYTDotNetCore.MvcAPP2.Models;
+using Newtonsoft.Json;
+using Refit;
+
+namespace MYTDotNetCore.MvcAPP2.Controllers;
+
+public class BlogRefitController : Controller
+{
+    private readonly IBlogApi _blogApi;
+
+    public BlogRefitController(IBlogApi blogApi)
+    {
+        _blogApi = blogApi;
+    }
+
+    [ActionName("Index")]
+    public async Task<IActionResult> BlogIndex(int pageNo = 1, int pageSize = 10)
+    {
+        var model = await _blogApi.GetBlogs(pageNo, pageSize);
+        return View("BlogIndex", model);
+    }
+
+    [ActionName("Create")]
+    public IActionResult BlogCreate()
+    {
+        return View("BlogCreate");
+    }
+
+    [HttpPost]
+    [ActionName("Save")]
+    public async Task<IActionResult> BlogSave(BlogModel blog)
+    {
+        await _blogApi.CreateBlog(blog);
+        return Redirect("/Blog");
+    }
+
+    [ActionName("Edit")]
+    public async Task<IActionResult> BlogEdit(int id)
+    {
+        var model = await _blogApi.GetBlog(id);
+        return View("BlogEdit", model);
+    }
+
+    [ActionName("Update")]
+    public async Task<IActionResult> BlogUpdate(int id, BlogModel blog)
+    {
+        var model = _blogApi.UpdateBlog(id, blog);
+        return Redirect("/Blog");
+    }
+
+    [ActionName("Delete")]
+    public async Task<IActionResult> BlogDelete(int id)
+    {
+        await _blogApi.DeleteBlog(id);
+        return Redirect("/blog");
+    }
+}
---
+++ MYTDotNetCore.MvcAPP2/IBlogApi.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MYTDotNetCore.MvcAPP2.Models;
+using Refit;
+
+namespace MYTDotNetCore.MvcAPP2;
+
+public interface IBlogApi
+{
+    [Get("/api/blog/{pageNo}/{pageSize}")]
+    Task<BlogResponseModel> GetBlogs(int pageNo, int pageSize);
+
+    [Get("/api/blog/{id}")]
+    Task<BlogModel> GetBlog(int id);
+
+    [Post("/api/blog")]
+    Task<string> CreateBlog(BlogModel blog);
+
+    [Put("/api/blog/{id}")]
+    Task<string> UpdateBlog(int id, BlogModel blog);
+
+    [Patch("/api/blog/{id}")]
+    Task<string> PatchBlog(int id, BlogModel blog);
+
+    [Delete("/api/blog/{id}")]
+    Task<string> DeleteBlog(int id);
+}
