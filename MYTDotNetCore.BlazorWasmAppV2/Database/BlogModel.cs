fd823db8a2ab03e572df91775d67a250610061e7
Moe Yan Htun <moeyan227970@gmail.com>
2024-06-12 22:31:15 +0630

    [Complete] Blazor WasmApp CRUD

---
+++ MYTDotNetCore.BlazorWasmAppV2/Database/BlogModel.cs
@@ -0,0 +1,10 @@
+﻿namespace MYTDotNetCore.BlazorWasmAppV2.Database
+{
+    public class BlogModel
+    {
+        public string BlogId { get; set; }
+        public string BlogTitle { get; set; }
+        public string BlogAuthor { get; set; }
+        public string BlogContent { get; set; }
+    }
+}
