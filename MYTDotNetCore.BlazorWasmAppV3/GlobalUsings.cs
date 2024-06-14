d5081fd745cca7549bf22aadee753d0e038c5298
Moe Yan Htun <moeyan227970@gmail.com>
2024-06-14 20:42:27 +0630

    -

---
+++ MYTDotNetCore.BlazorWasmAppV3/GlobalUsings.cs
@@ -0,0 +1,6 @@
+﻿// Global using directives
+
+global using Microsoft.AspNetCore.Components;
+global using MudBlazor;
+global using MYTDotNetCore.BlazorWasmAppV3.Database;
+global using MYTDotNetCore.BlazorWasmAppV3.Services;
