15b2217f5c9048fde96787aaa57ef7e0a00b1b15
Moe Yan Htun <moeyan227970@gmail.com>
2024-06-18 23:47:17 +0630

    -

---
+++ MYTDotNetCore.MinimalApi/BlogServices.cs
@@ -0,0 +1,11 @@
+﻿namespace MYTDotNetCore.MinimalApi;
+
+public class MinimalApi
+{
+    
+    private static string EndPoint = "/api/blog";
+    public static IEndpointRouteBuilder  MapMethod(this IEndpointRouteBuilder app)
+    {
+        app.MapGet(EndPoint, async (AppDbContext db))
+    }
+}
