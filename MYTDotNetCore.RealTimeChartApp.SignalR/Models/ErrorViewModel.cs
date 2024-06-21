c095d125b3ae0abf4425db9320bc6ffff80e648c
Moe Yan Htun <moeyan227970@gmail.com>
2024-06-21 17:34:11 +0630

    [Complete] Realtime ChartApp SignalR

---
+++ MYTDotNetCore.RealTimeChartApp.SignalR/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MYTDotNetCore.RealTimeChartApp.SignalR.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
