a39f74d33a75abe668f7b48ba2af9858d72a0236
Moe Yan Htun <moeyan227970@gmail.com>
2024-06-23 17:15:15 +0630

    [Complete] LoginPage setup Middleware

---
+++ MYTDotNetCore.MvcApp3/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MYTDotNetCore.MvcApp3.Controllers;
+
+public class LoginController : Controller
+{
+    [ActionName("Index")]
+    public IActionResult LoginIndex()
+    {
+        return View("LoginIndex");
+    }
+}
