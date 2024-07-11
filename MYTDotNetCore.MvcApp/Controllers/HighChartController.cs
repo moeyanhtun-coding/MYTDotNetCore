0c99e2870eaf4055736d799ed3d9a7687b05bc02
Moe Yan Htun <moeyan227970@gmail.com>
2024-07-11 19:49:29 +0630

    [Complete] Add HighChartController

---
+++ MYTDotNetCore.MvcApp/Controllers/HighChartController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MYTDotNetCore.MvcApp.Models;
+
+namespace MYTDotNetCore.MvcApp.Controllers
+{
+    public class HighChartController : Controller
+    {
+        public IActionResult StackedBarChart()
+        {
+            HighChartStackedBarChartModel model = new HighChartStackedBarChartModel()
+            {
+                Category = new List<MonthData>
+                {
+                    new MonthData
+                    {
+                        categories = new List<string>
+                        {
+                            "January",
+                            "February",
+                            "March",
+                            "April",
+                            "May"
+                        },
+                    }
+                },
+                Serires = new List<VehicleData>
+                {
+                    new VehicleData
+                    {
+                        name = "Motorcycles",
+                        data = new List<int> { 74, 27, 52, 93, 1272 }
+                    },
+                    new VehicleData
+                    {
+                        name = "Null-emission vehicles",
+                        data = new List<int> { 2106, 2398, 3046, 3195, 4916 }
+                    },
+                    new VehicleData
+                    {
+                        name = "Conventional vehicles",
+                        data = new List<int> { 12213, 12721, 15242, 16518, 25037 }
+                    }
+                }
+            };
+
+            return View(model);
+        }
+    }
+}
