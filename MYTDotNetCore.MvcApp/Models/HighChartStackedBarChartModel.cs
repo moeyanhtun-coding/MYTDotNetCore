65948a5d401d2d6105b53df39462a6f4b804db37
Moe Yan Htun <moeyan227970@gmail.com>
2024-07-11 19:50:59 +0630

    Reapply "[Complete] Add HighChartStackedBarChart Model"
    This reverts commit 5f5dae0cbcaf50f8cf93539c192655b970cbeea0.

---
+++ MYTDotNetCore.MvcApp/Models/HighChartStackedBarChartModel.cs
@@ -0,0 +1,18 @@
+﻿namespace MYTDotNetCore.MvcApp.Models;
+
+public class HighChartStackedBarChartModel
+{
+    public List<MonthData> Category { get; set; }
+    public List<VehicleData> Serires { get; set; }
+}
+
+public class VehicleData
+{
+    public string name { get; set; }
+    public List<int> data { get; set; }
+}
+
+public class MonthData
+{
+    public List<string> categories { get; set; }
+}
