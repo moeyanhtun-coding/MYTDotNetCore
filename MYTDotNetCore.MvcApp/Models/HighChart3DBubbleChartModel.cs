4a0af32550a0ff38b77c5b2840d6716d27919e70
Moe Yan Htun <moeyan227970@gmail.com>
2024-07-11 21:01:57 +0630

    [Complete] 3DBubbleChart Model

---
+++ MYTDotNetCore.MvcApp/Models/HighChart3DBubbleChartModel.cs
@@ -0,0 +1,9 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace MYTDotNetCore.MvcApp.Models;
+
+public class HighChart3DBubbleChartModel
+{
+    public int[][] data1 { get; set; }
+    public int[][] data2 { get; set; }
+}
